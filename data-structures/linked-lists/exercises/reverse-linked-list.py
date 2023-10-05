22fed85061c4f7bafaa0ffd5506e2e82bf5460f7
Danny França <me@dannyfranca.com>
2023-10-05 08:14:12 +0100

    feat: add reverse linked list recursive with no mutation

---
+++ data-structures/linked-lists/exercises/reverse-linked-list.py
@@ -0,0 +1,35 @@
+class Node(object):
+    def __init__(self, value, next=None):
+        self.value = value
+        self.next = next
+
+    def __str__(self) -> str:
+        curr = self
+        output = ""
+        while curr:
+            output += str(curr.value) + "->"
+            curr = curr.next
+        output += "NULL"
+        return output
+
+
+def reverse_iterative(node):
+    curr = node
+    head = Node(curr.value)
+    while curr:
+        next = curr.next
+        if next:
+            prev = head
+            head = Node(next.value, prev)
+        curr = next
+    return head
+
+
+def reverse_recursive(node):
+    head = Node
+
+
+ll = Node(1, Node(2, Node(3, Node(4, Node(5)))))
+print(ll)
+print(reverse_iterative(ll))
+print(ll)
