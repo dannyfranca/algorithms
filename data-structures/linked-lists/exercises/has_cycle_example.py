773d8cb1301fdf5ae3274129f80e7a20bf5b5fa7
Danny França <me@dannyfranca.com>
2023-10-07 10:28:11 +0100

    refactor: add LinkedListWithCycle

--- data-structures/linked-lists/exercises/has_cycle_example.py
+++ data-structures/linked-lists/exercises/has_cycle_example.py
@@ -1,9 +1,15 @@
+from node import LinkedListWithCycle
 from has_cycle import has_cycle
 
 
-ll = LinkedList(1, LinkedList(2, LinkedList(3, LinkedList(4))))
+ll = LinkedListWithCycle(
+    1, LinkedListWithCycle(2, LinkedListWithCycle(3, LinkedListWithCycle(4)))
+)
 print(has_cycle(ll))
 
-## Create cycle
+# Create cycle
 ll.next.next.next.next = ll
 print(has_cycle(ll))
+
+# Print cycle
+print(ll)
--- data-structures/linked-lists/exercises/node.py
+++ data-structures/linked-lists/exercises/node.py
@@ -1,3 +1,6 @@
+from weakref import WeakKeyDictionary
+
+
 class LinkedList:
     def __init__(self, value, next=None):
         self.value = value
@@ -8,6 +11,27 @@ class LinkedList:
         output = ""
         while curr:
             output += str(curr.value) + "->"
+            curr = curr.next
+        if output.endswith("->"):
+            return output[:-2]
+        return output
+
+
+class LinkedListWithCycle:
+    def __init__(self, value, next=None):
+        self.value = value
+        self.next = next
+
+    def __str__(self):
+        curr = self
+        output = ""
+        read_dict = WeakKeyDictionary()
+        while curr:
+            if read_dict.get(curr):
+                output += "(" + str(curr.value) + "...)"
+                break
+            output += str(curr.value) + "->"
+            read_dict[curr] = True
             curr = curr.next
         if output.endswith("->"):
             return output[:-2]
