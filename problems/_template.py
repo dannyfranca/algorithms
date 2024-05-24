64c3a26dfa746b06e7fd6283b7e8afe2c9767490
Danny Franca <me@dannyfranca.com>
2024-05-24 07:57:52 +0100

    feat: add problem template

---
+++ problems/_template.py
@@ -0,0 +1,21 @@
+# tags: linked_list
+import unittest
+
+
+class Solution:
+    def versionRecursive(self):
+        return None
+
+    def versionIterative(self):
+        return None
+
+
+class TestSolution(unittest.TestCase):
+    def test_name(self):
+        # prepare
+        self.assertEqual(Solution().versionIterative(), None)
+        self.assertEqual(Solution().versionRecursive(), None)
+
+
+if __name__ == "__main__":
+    unittest.main()
